63a371c485bec6b4ecbb8291841735eeda30101e
lierzhe <lierzhezzz@163.com>
2020-08-27 13:18:40 +0800

    init repo

---
+++ src/test/java/com/sjzc/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjzc.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
